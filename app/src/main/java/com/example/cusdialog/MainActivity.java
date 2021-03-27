bc184169a8a7e7735709586d42c4ca24429ec0a4
ThanhTon115 <thanhtontran115@gmail.com>
2021-03-27 09:11:08 +0700

    custom dialog

---
+++ app/src/main/java/com/example/cusdialog/MainActivity.java
@@ -0,0 +1,74 @@
+package com.example.cusdialog;
+
+import android.content.DialogInterface;
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.LayoutInflater;
+import android.view.View;
+
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+                displayAlertDialog();
+            }
+        });
+    }
+
+    private void displayAlertDialog() {
+        LayoutInflater inflater = getLayoutInflater();
+        View alartLayout = inflater.inflate(R.layout.layout_dialog,null);
+        //control
+        AlertDialog.Builder alert = new AlertDialog.Builder(this);//khởi tạo alert
+        alert.setView(alartLayout);
+        alert.setCancelable(true);
+        alert.setPositiveButton("ADD",(dialog, which) -> {});
+        alert.setNegativeButton("CANCEL",(dialog, which) -> {});
+        AlertDialog dialog = alert.create();
+        dialog.show();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
